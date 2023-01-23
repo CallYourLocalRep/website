6470c0cae382009028b774d9bf539becf5ebb003
Arnab Ghosh <arnabcare21@gmail.com>
2023-01-22 17:16:20 -0800

    Start work on routing

--- app/routes/issues/$issue.tsx
+++ app/routes/issues/$issue.tsx
@@ -1,4 +1,11 @@
+import { LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async ({ params }) => {
+    const slug = params.issue;
+}
+
 export default function Issue() {
+
     return (
         <p>A very pressing issue</p>
     )
